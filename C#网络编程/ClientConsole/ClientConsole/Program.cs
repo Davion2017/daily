f4aa96ce18ff3d3ba7c51a7dcc04c0ff772f0a52
Davion <davion2017@foxmail.com>
2019-06-17 11:24:17 +0800

    初步了解网络编程

---
+++ C#网络编程/ClientConsole/ClientConsole/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ClientConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Client is running……");
+
+            //单客户端连接服务端
+            TcpClient client = new TcpClient();
+            try
+            {
+                client.Connect("localhost", 8500);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine("Server Connected!\n{0}-->{1}", client.Client.LocalEndPoint, client.Client.RemoteEndPoint);
+
+            //string msg = "\"Welcome To TraceFact.Net\"";
+            NetworkStream streamToServer = client.GetStream();
+
+            ConsoleKey key;
+            Console.WriteLine("Menu: S - Send, X - Exit");
+            do
+            {
+                key = Console.ReadKey(true).Key;
+                if(key == ConsoleKey.S)
+                {
+                    Console.Write("Input the message：");
+                    String msg = Console.ReadLine();
+                    byte[] buffer = Encoding.Unicode.GetBytes(msg);     // 获得缓存
+                    streamToServer.Write(buffer, 0, buffer.Length);     // 发往服务器
+                    Console.WriteLine("Sent: {0}", msg);
+                }
+            } while (key != ConsoleKey.X);
+
+            
+
+            //多客户端连接服务端
+            //TcpClient client;
+            //for (int i = 0; i < 2; i++)
+            //{
+            //    try
+            //    {
+            //        client = new TcpClient();
+            //        client.Connect("localhost", 8500);
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        Console.WriteLine(ex.Message);
+            //        return;
+            //    }
+            //    Console.WriteLine("Server Connected!\n{0}-->{1}", client.Client.LocalEndPoint, client.Client.RemoteEndPoint);
+            //}
+
+
+            Console.WriteLine("\n\n输入\"Q\"键退出。");
+            //ConsoleKey key;
+            do
+            {
+                key = Console.ReadKey(true).Key;
+            } while (key != ConsoleKey.Q);
+        }
+    }
+}
---
+++ C#网络编程/ServerConsole/ServerConsole/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ServerConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int BufferSize = 8192;
+            Console.WriteLine("Server is running……");
+            IPAddress ip = new IPAddress(new byte[] { 127, 0, 0, 1 });
+            TcpListener listener = new TcpListener(ip, 8500);
+            listener.Start();
+            Console.WriteLine("Start Listening");
+            
+            TcpClient remoteClient = listener.AcceptTcpClient();
+            Console.WriteLine("Client Connected! {0}<--{1}", remoteClient.Client.LocalEndPoint, remoteClient.Client.RemoteEndPoint);
+
+
+            NetworkStream streamToClient = remoteClient.GetStream();
+            do
+            {
+                byte[] buffer = new byte[BufferSize];
+                int bytesRead = streamToClient.Read(buffer, 0, BufferSize);
+                Console.WriteLine("Reading data, {0} bytes ...", bytesRead);
+
+                string msg = Encoding.Unicode.GetString(buffer, 0, bytesRead);
+                Console.WriteLine("Received: {0}", msg);
+            } while (true);
+            
+
+            Console.WriteLine("\n\n输入\"Q\"键退出。");
+            ConsoleKey key;
+            do
+            {
+                key = Console.ReadKey(true).Key;
+            } while (key != ConsoleKey.Q);
+        }
+    }
+}
